3259cc994663f2a2ae92bc07ae7823e0c03b312b
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2024-05-05 09:37:25 -0700

    Add client program that also uses mio
    Both client and server programs currently have issues. See README.md for details

---
+++ README.md
@@ -0,0 +1,8 @@
+# Arkanox
+
+Echo server and client in Rust using mio using an event loop
+
+# Issues
+
+[ ] Client does not receive all messages back from server
+[ ] Server crashes when client is killed (does not happen when using `nc` for example)
--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,16 +1,83 @@
-use std::{
-    io::{Read, Write},
-    net::TcpStream,
-};
+use std::io::{ErrorKind, Read, Write};
+
+use mio::{net::TcpStream, Events, Interest, Poll, Token};
+
+const CLIENT: Token = Token(0);
+const MAX_MSGS: usize = 10;
 
 fn main() -> anyhow::Result<()> {
-    let mut connection = TcpStream::connect("127.0.0.1:9000")?;
+    let mut connection = TcpStream::connect("127.0.0.1:9000".parse()?)?;
 
-    connection.write_all("hello world!".as_bytes())?;
-    connection.flush()?;
-    println!("Sent message to server");
-    let mut buf = vec![0; 12];
-    connection.read_exact(&mut buf)?;
-    println!("Received {} from server", std::str::from_utf8(&buf)?);
-    Ok(())
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(1024);
+
+    poll.registry().register(
+        &mut connection,
+        CLIENT,
+        Interest::WRITABLE.add(Interest::READABLE),
+    )?;
+
+    let mut write_count = 0;
+    let mut read_count = 0;
+
+    // main event loop
+    loop {
+        poll.poll(&mut events, None)?;
+
+        for event in events.iter() {
+            if event.token().0 != CLIENT.0 {
+                panic!("Got an event for unregistered token!");
+            }
+
+            if event.is_writable() {
+                match connection.peer_addr() {
+                    Ok(_) => {
+                        if write_count < MAX_MSGS {
+                            let msg = format!("[{}] hello world!", write_count);
+                            // TODO: We dont handle the case where we werent able to write the full message
+                            match connection.write(msg.as_bytes()) {
+                                Ok(_n) => {
+                                    write_count += 1;
+                                }
+                                Err(e) => {
+                                    println!("Error {} when writing to socket", e);
+                                }
+                            }
+                        }
+                    }
+                    Err(ref e) if e.raw_os_error() == Some(libc::EINPROGRESS) => continue,
+                    Err(e) if e.kind() == ErrorKind::NotConnected => continue,
+                    Err(e) if e.kind() == ErrorKind::WouldBlock => continue,
+                    Err(_) => {
+                        panic!("Could not connect to server");
+                    }
+                }
+            }
+
+            if event.is_readable() {
+                let mut buf = vec![0; 16];
+                let mut connection_closed = false;
+                match connection.read(&mut buf) {
+                    Ok(0) => {
+                        println!("0 byte read");
+                        connection_closed = true;
+                    }
+                    Ok(_) => {
+                        println!("Received {} from server", String::from_utf8(buf)?);
+                        read_count += 1;
+                        if read_count == MAX_MSGS {
+                            return Ok(());
+                        }
+                    }
+                    Err(e) if e.kind() == ErrorKind::WouldBlock => continue,
+                    Err(e) if e.kind() == ErrorKind::Interrupted => continue,
+                    Err(e) => panic!("Error {} when reading from socket", e),
+                }
+
+                if connection_closed {
+                    return Ok(());
+                }
+            }
+        }
+    }
 }
